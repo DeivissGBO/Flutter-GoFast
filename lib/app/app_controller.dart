cbe885d00bf35fba0c9a1b522f0844c5d4c009d2
DeivissGBO <39034525+DeivissGBO@users.noreply.github.com>
2020-04-26 23:31:58 -0400

    Theme control

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,1 +1,26 @@
-class AppController {}
+import 'package:flutter/material.dart';
+import 'package:flutter_gofast/app/core/theme/app_theme_dark.dart';
+import 'package:flutter_gofast/app/core/theme/app_theme_light.dart';
+import 'package:mobx/mobx.dart';
+
+import 'core/interfaces/theme_app_interface.dart';
+part 'app_controller.g.dart';
+
+class AppController = AppControllerBase with _$AppController;
+
+abstract class AppControllerBase with Store {
+  @observable
+  IThemeAppInterface themeApp = AppThemeLight();
+  @action
+  setThemeData(ThemeMode themeMode) {
+    switch (themeMode) {
+      case ThemeMode.dark:
+        themeApp = AppThemeDark();
+        break;
+
+      default:
+        themeApp = AppThemeLight();
+        break;
+    }
+  }
+}
--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gofast/app/app_controller.dart';
 import 'package:flutter_gofast/app/core/intercionalization/app_localizations.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -7,32 +10,32 @@ class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter GoFast',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      initialRoute: '/',
-      navigatorKey: Modular.navigatorKey,
-      onGenerateRoute: Modular.generateRoute,
-      supportedLocales: [
-        Locale('en', 'US'),
-        Locale('pt', 'BR'),
-      ],
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate
-      ],
-      localeResolutionCallback: (locale, supportedLocales) {
-        for (var supportedLocale in supportedLocales) {
-          if (supportedLocale.languageCode == locale.languageCode &&
-              supportedLocale.countryCode == locale.countryCode) {
-            return supportedLocale;
+    return Observer(builder: (context) {
+      return MaterialApp(
+        title: 'Flutter GoFast',
+        theme: Modular.get<AppController>().themeApp.getTheme(),
+        initialRoute: '/',
+        navigatorKey: Modular.navigatorKey,
+        onGenerateRoute: Modular.generateRoute,
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('pt', 'BR'),
+        ],
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate
+        ],
+        localeResolutionCallback: (locale, supportedLocales) {
+          for (var supportedLocale in supportedLocales) {
+            if (supportedLocale.languageCode == locale.languageCode &&
+                supportedLocale.countryCode == locale.countryCode) {
+              return supportedLocale;
+            }
           }
-        }
-        return supportedLocales.first;
-      },
-    );
+          return supportedLocales.first;
+        },
+      );
+    });
   }
 }
